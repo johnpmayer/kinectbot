b483d2ba742dc750e42a3f19ad7235d60c9c523b
John P Mayer, Jr <john.p.mayer.jr@gmail.com>
2011-11-22 17:24:03 -0500

    WARNING! Safe....

--- demo/kinect/libfree-self-test/self-test.c
+++ demo/kinect/libfree-self-test/self-test.c
@@ -14,6 +14,11 @@
 
 #define REGION_RES 80
 
+// some value between 0 and 255
+#define THRESH 225
+
+#define LOOP 1
+
 uint32_t get_region(uint32_t offset)
 {
   
@@ -30,15 +35,23 @@ uint32_t get_region(uint32_t offset)
   
 }
 
+uint16_t t_gamma[2048];
+
+void init(void) {
+  int i;
+  for (i=0; i<2048; i++) {
+    float v = i/2048.0;
+    v = powf(v, 3)* 6;
+    t_gamma[i] = v*6*256;
+  }
+}
+
 int main()
 {
   
-  uint16_t* data;
-  char* picture = calloc(W*H*3, sizeof(uint8_t));
+  init();
   
-  uint64_t* regions = calloc( (W / REGION_RES) * (H / REGION_RES), sizeof(uint64_t) );
   
-  uint32_t timestamp;
   
   printf("Start\n");  
   
@@ -46,18 +59,26 @@ int main()
   
   do {
     
+    uint16_t* data;
+    char* picture = calloc(W*H*3, sizeof(uint8_t));
+    
+    uint64_t* regions = calloc( (W / REGION_RES) * (H / REGION_RES), sizeof(uint64_t) );
+    
+    uint32_t timestamp;
+    
     int32_t err = freenect_sync_get_depth( (void**)(&data), 
 			     &timestamp, 
 			     0, 
 			     FREENECT_DEPTH_11BIT );
     
-    printf("Get depth return code: %" PRIi32 "\n", err);
+    //printf("Get depth return code: %" PRIi32 "\n", err);
     
     if (err) {
+      printf("can't access kinect\n");
       return err;
     }
     
-    printf("Got data form depth camera\n");
+    //printf("Got data form depth camera\n");
     
     uint32_t offset;
     uint32_t max = W*H;
@@ -68,19 +89,72 @@ int main()
       //uint16_t tags = GET5(data[offset]);
       uint16_t depth = GET11(data[offset]);
       
-      uint8_t pixel = depth >> 3;
+      uint16_t pval = t_gamma[depth];
+      int lb = pval & 0xff;
       
       if ( (offset % REGION_RES == 0) || ((offset/W) % REGION_RES == 0) ) {
 	picture[3*offset] = -1;
 	picture[3*offset+1] = -1;
 	picture[3*offset+2] = -1;
       } else if (depth < 2047) {
-	picture[3*offset] = pixel;
-	picture[3*offset+1] = pixel;
-	picture[3*offset+2] = pixel;
+	
+#ifdef COLOR
+        switch (pval>>8) {
+	case 0:
+	  picture[3*offset+0] = 255;
+	  picture[3*offset+1] = 255-lb;
+	  picture[3*offset+2] = 255-lb;
+	  break;
+	case 1:
+	  picture[3*offset+0] = 255;
+	  picture[3*offset+1] = lb;
+	  picture[3*offset+2] = 0;
+	  break;
+	case 2:
+	  picture[3*offset+0] = 255-lb;
+	  picture[3*offset+1] = 255;
+	  picture[3*offset+2] = 0;
+	  break;
+	case 3:
+	  picture[3*offset+0] = 0;
+	  picture[3*offset+1] = 255;
+	  picture[3*offset+2] = lb;
+	  break;
+	case 4:
+	  picture[3*offset+0] = 0;
+	  picture[3*offset+1] = 255-lb;
+	  picture[3*offset+2] = 255;
+	  break;
+	case 5:
+	  picture[3*offset+0] = 0;
+	  picture[3*offset+1] = 0;
+	  picture[3*offset+2] = 255-lb;
+	  break;
+	default:
+	  picture[3*offset+0] = 0;
+	  picture[3*offset+1] = 0;
+	  picture[3*offset+2] = 0;
+	  break;
+	  }
+#else
+	
+	picture[3*offset] = pval >> 3;
+	picture[3*offset+1] = pval >> 3;
+	picture[3*offset+2] = pval >> 3;
+	
+#endif
+	
       }
       
-      regions[get_region(offset)] += depth;
+#ifdef COLOR
+      /* grep red-scale */
+      picture[3*offset+1] = picture[3*offset];
+      picture[3*offset+2] = picture[3*offset];
+#endif
+      
+      uint8_t pixel = picture[3*offset];
+      regions[get_region(offset)] += pixel;
+      //picture[3*offset];
       
       //printf("%" PRIx32 ":\t%" PRIu16 "\n", offset, data[offset]);
       
@@ -100,22 +174,38 @@ int main()
 	    );
     */
     
-  } while (0);
-  
-  printf("Regions:\n");
-  int i,j;
-  for (i = 0; i < (H / REGION_RES); i++) {
-    for (j = 0; j < (W / REGION_RES); j++) {
-      
-      uint64_t region_avg = regions[(i * (W / REGION_RES)) + j] / pow(REGION_RES, 2);
-      
-      printf("%" PRIu64 "\t", region_avg);
-      
+    //printf("Regions:\n");
+    int obstacle = 0;
+    int i,j;
+    for (i = 0; !obstacle && i < (H / REGION_RES); i++) {
+      for (j = 0; !obstacle && j < (W / REGION_RES); j++) {
+	
+	uint64_t region_avg = regions[(i * (W / REGION_RES)) + j] / pow(REGION_RES, 2);
+	if (region_avg > THRESH){
+	  obstacle = 1;
+	}
+	//printf("%" PRIu64 "\t", region_avg);
+	
+      }
+      //printf("\n");
     }
-    printf("\n");
-  }
+    
+    if(obstacle) {
+      printf("WARNING!\n");
+    } else {
+      printf("Safe....\n");
+    }
+    
+    free(picture);
+    free(regions);
+    
+  } while (LOOP);
   
-  write_bmp("output.bmp", W, H, picture);
+  
+  
+  
+  
+  //write_bmp("output.bmp", W, H, picture);
   
   return 0;
   
