e44b0cd87e1934518a5c6939cb52c3e31313e9ee
John P Mayer, Jr <john.p.mayer.jr@gmail.com>
2011-12-10 12:55:06 -0500

    much better kinect region debug

--- demo/kinect/libfree-self-test/self-test.c
+++ demo/kinect/libfree-self-test/self-test.c
@@ -16,7 +16,7 @@
 #define REGION_RES 80
 
 // some value between 0 and 255
-#define THRESH 225
+#define THRESH 200
 
 #define LOOP 1
 
@@ -62,147 +62,112 @@ int main()
     return 1;
   }
   
-  //printf("Start\n");  
   mvaddstr(3,3,"Start\n");
   refresh();
   
-  do {
-    
-    uint16_t* data;
-    char* picture = calloc(W*H*3, sizeof(uint8_t));
-    
-    uint64_t* regions = calloc( (W / REGION_RES) * (H / REGION_RES), sizeof(uint64_t) );
-    
-    uint32_t timestamp;
-    
-    int32_t err = freenect_sync_get_depth( (void**)(&data), 
-			     &timestamp, 
-			     0, 
-			     FREENECT_DEPTH_11BIT );
-    
-    if (err) {
-      delwin(win);
-      endwin();
-      refresh();
-
-      printf("can't access kinect\n");
-      exit(1);
-    }
-    
-    uint32_t offset;
-    uint32_t max = W*H;
-    
-    for(offset = 0; offset < max; offset ++ ){
+  do 
+    {
       
-      //uint16_t tags = GET5(data[offset]);
-      uint16_t depth = GET11(data[offset]);
+      uint16_t* data;
       
-      uint16_t pval = t_gamma[depth];
-      int lb = pval & 0xff;
+      uint64_t* regions = calloc( (W / REGION_RES) * (H / REGION_RES), 
+				  sizeof(uint64_t) );
       
-      if ( (offset % REGION_RES == 0) || ((offset/W) % REGION_RES == 0) ) {
-	picture[3*offset] = -1;
-	picture[3*offset+1] = -1;
-	picture[3*offset+2] = -1;
-      } else if (depth < 2047) {
+      uint32_t timestamp;
+      
+      int32_t err = freenect_sync_get_depth( (void**)(&data), 
+					     &timestamp, 
+					     0, 
+					     FREENECT_DEPTH_11BIT );
+      
+      if (err) {
+	delwin(win);
+	endwin();
+	refresh();
 	
-        switch (pval>>8) {
-	case 0:
-	  picture[3*offset+0] = 255;
-	  picture[3*offset+1] = 255-lb;
-	  picture[3*offset+2] = 255-lb;
-	  break;
-	case 1:
-	  picture[3*offset+0] = 255;
-	  picture[3*offset+1] = lb;
-	  picture[3*offset+2] = 0;
-	  break;
-	case 2:
-	  picture[3*offset+0] = 255-lb;
-	  picture[3*offset+1] = 255;
-	  picture[3*offset+2] = 0;
-	  break;
-	case 3:
-	  picture[3*offset+0] = 0;
-	  picture[3*offset+1] = 255;
-	  picture[3*offset+2] = lb;
-	  break;
-	case 4:
-	  picture[3*offset+0] = 0;
-	  picture[3*offset+1] = 255-lb;
-	  picture[3*offset+2] = 255;
-	  break;
-	case 5:
-	  picture[3*offset+0] = 0;
-	  picture[3*offset+1] = 0;
-	  picture[3*offset+2] = 255-lb;
-	  break;
-	default:
-	  picture[3*offset+0] = 0;
-	  picture[3*offset+1] = 0;
-	  picture[3*offset+2] = 0;
-	  break;
-	  }
-	
+	printf("can't access kinect\n");
+	exit(1);
       }
       
-      picture[3*offset+1] = picture[3*offset];
-      picture[3*offset+2] = picture[3*offset];
+      uint32_t offset;
+      uint32_t max = W*H;
       
-      uint8_t pixel = picture[3*offset];
-      regions[get_region(offset)] += pixel;
-
-    }
-    
-    int obstacle = 0;
-    int i,j;
-    int base_row = 7;
-    int base_col = 5;
-    for (i = 0; i < (H / REGION_RES); i++) {
+      for(offset = 0; offset < max; offset ++ )
+	{
+	  
+	  uint16_t depth = GET11(data[offset]);
+	  
+	  uint16_t pval = t_gamma[depth];
+	  int lb = pval & 0xff;
+	  
+	  /*
+	   * what we really want here is only the closest
+	   * detected objects, so we only look at case 0:
+	   */
+	  uint8_t pixel;// = picture[3*offset];
+	  
+	  if (pval>>8 == 0) {
+	    pixel = 255-lb;
+	  } else {
+	    pixel = 0;
+	  }
+	  
+	  regions[get_region(offset)] += pixel;
+	}
       
-      mvaddstr(base_row + 2 * i, base_col, "|");
       
-      for (j = 0; j < (W / REGION_RES); j++) {
+      int obstacle = 0;
+      int i,j;
+      int base_row = 7;
+      int base_col = 5;
+      for (i = 0; i < (H / REGION_RES); i++) {
 	
-	uint64_t region_avg = 
-	  regions[(i * (W / REGION_RES)) + j] / pow(REGION_RES, 2);
+	mvaddstr(base_row + 3 * i, base_col, "|");
 	
-	char* cursesChar = " ";
-	
-	if (region_avg > THRESH) {
-	  obstacle = 1;
-	  cursesChar = "@";
+	for (j = 0; j < (W / REGION_RES); j++) {
+	  
+	  uint64_t region_avg = 
+	    regions[(i * (W / REGION_RES)) + j] / pow(REGION_RES, 2);
+	  
+	  char buf[16];
+	  
+	  if (region_avg > THRESH) 
+	    {
+	      obstacle = 1;
+	      sprintf(buf, "(%" PRIu64 ")    ", region_avg);
+	    }
+	  else
+	    {
+	      sprintf(buf, "%" PRIu64 "    ", region_avg);
+	    }
+	  
+	  
+	  mvaddstr( base_row + 3 * i,
+		    base_col + 6 * (j+1),
+		    buf );
+	  
+	  
+	  
 	}
 	
-	mvaddstr(base_row + 2 * i,
-		 base_col + 4 * (j+1),
-		 cursesChar);
-	
+	mvaddstr( base_row + 3 * i, 
+		  base_col + 6 * ((W/REGION_RES)+1), 
+		  "|");
 	
       }
       
-      mvaddstr(base_row + 2 * i, base_col + 4 * ((W/REGION_RES)+1), "|");
-
-    }
+      if(obstacle) {
+	mvaddstr(5,3,"WARNING!\n");
+      } else {
+	mvaddstr(5,3,"Safe....\n");
+      }
+      
+      refresh();
+      
+      free(regions);
+      
+    } while (LOOP);
     
-    if(obstacle) {
-      mvaddstr(5,3,"WARNING!\n");
-    } else {
-      mvaddstr(5,3,"Safe....\n");
-    }
-    refresh();
-    
-    free(picture);
-    free(regions);
-    
-  } while (LOOP);
-  
-  
-  
-  
-  
-  //write_bmp("output.bmp", W, H, picture);
-  
   delwin(win);
   endwin();
   refresh();
